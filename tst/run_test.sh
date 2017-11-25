6253459d2f4cd1df1de1a732f831068f3699f9c2
sakellar <angsakel@hotmail.com>
2017-11-25 22:49:18 +0200

    update run_test.sh removed comments

--- tst/run_test.sh
+++ tst/run_test.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=.
 PYTHONPATH=$PYTHONPATH:../src
 python -m unittest test_parser 
-#python -m unittest test_format_output 
-#python -m unittest test_collector 
+python -m unittest test_format_output 
+python -m unittest test_collector 
