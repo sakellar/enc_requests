2529dee9b93e00f1b4f09179bf2bcc50b5183575
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-25 22:45:45 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='python_assignment',
+   version='1.0',
+   description='python-assignment',
+   author='Angelos Sakellaropoulos',
+   author_email='angsakel@gmail.com',
+   python_requires='==2.7',
+   install_requires=['mock'],
+)
