b9dce89ae2e492bd29a40eda1677c8d1055565fa
Angelos Sakellaropoulos <angelos.sakellaropoulos@nokia.com>
2017-11-26 12:19:23 +0200

    changes to setup.py packages

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='python_assignment',
+   name='encode-python-assignment',
    version='1.0',
+   packages=['src','tst'],
    description='python-assignment',
    author='Angelos Sakellaropoulos',
    author_email='angsakel@gmail.com',
    python_requires='==2.7',
-   install_requires=['mock'],
+   install_requires=['mock', 'requests'],
 )
